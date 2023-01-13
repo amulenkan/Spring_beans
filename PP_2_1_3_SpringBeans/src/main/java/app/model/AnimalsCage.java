c82cfa7db48d8d23a8762fc44ebcbcfe734795ab
amulenkan <115099530+amulenkan@users.noreply.github.com>
2023-01-13 23:01:03 +0300

    au

--- PP_2_1_3_SpringBeans/src/main/java/app/model/AnimalsCage.java
+++ PP_2_1_3_SpringBeans/src/main/java/app/model/AnimalsCage.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-import java.sql.ResultSet;
-
 @Component
 public class AnimalsCage {
 
